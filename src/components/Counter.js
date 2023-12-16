ad2bbdfc3254a06b040c53c261f342f687e1d11e
Kalin Hristov <kalin0169@gmail.com>
2023-12-16 14:41:45 +0200

    Counter Component can dispatch Action Object with 'type' and 'value' properties!

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -14,6 +14,10 @@ const Counter = () => {
     dispatch({ type: 'decrement' });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', value: 5 });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
@@ -22,6 +26,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
