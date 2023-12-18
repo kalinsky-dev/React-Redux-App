2d0867dccef807f32cf5463ed4d0aa5e5345e8f1
Kalin Hristov <kalin0169@gmail.com>
2023-12-18 14:37:56 +0200

    Render UserProfile and Auth Component conditionally whether I am Authenticated or Not!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 import Counter from './components/Counter';
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {isAuth && <UserProfile />}
+      {!isAuth && <Auth />}
       <Counter />
     </Fragment>
   );
