af5dd69851d328d93dbd49c4452132a337035051
Kalin Hristov <kalin0169@gmail.com>
2023-12-18 12:53:06 +0200

    Update reducer function with Redux Toolkit.

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,55 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'increment':
-      return { counter: state.counter + 1, showCounter: state.showCounter };
-    case 'decrement':
-      return { counter: state.counter - 1, showCounter: state.showCounter };
-    case 'increase':
-      return {
-        counter: state.counter + action.value,
-        showCounter: state.showCounter,
-      };
-    case 'toggle':
-      return {
-        counter: state.counter,
-        showCounter: !state.showCounter,
-      };
-    default:
-      return state;
-  }
-};
+// We can use createSlice to create a reducer and actions at the same time with help of redux toolkit!
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.value;
+    },
+    toggleCouter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-const store = createStore(counterReducer);
+// We use createReducer to create a reducer fungiont without redux toolkit!
+// const counterReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'increment':
+//       return { counter: state.counter + 1, showCounter: state.showCounter };
+//     case 'decrement':
+//       return { counter: state.counter - 1, showCounter: state.showCounter };
+//     case 'increase':
+//       return {
+//         counter: state.counter + action.value,
+//         showCounter: state.showCounter,
+//       };
+//     case 'toggle':
+//       return {
+//         counter: state.counter,
+//         showCounter: !state.showCounter,
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
+// const store = createStore(counterReducer);
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+
+export const counterActions = counterSlice.actions;
 export default store;
